cfdfc6dcfe896a429728f04699cab584f4bf174a
lalitakapadia <lalita.kapadia@gmail.com>
2024-03-13 16:33:51 +0000

    worken on schemas TypeDefs file and removed all duplicate file from Models

--- server/schemas/typeDefs.js
+++ server/schemas/typeDefs.js
@@ -1,14 +1,57 @@
-const typeDefs = `
-  type Class {
-    _id: ID
-    name: String
-    building: String
-    creditHours: Int
+const {gql} = require('apollo-server-express');
+
+const typeDefs = gql`
+  type User {
+    _id: ID!
+    username: String!
+    email: String!
+    phone: String!
+    addressLine1: String
+    addressLine2: String
+    addressLine3: String
+    city: String
+    postcode: String
+    county: String
+    country: String
+    userType: UserType!
+    expenses: [Expense]
+    budgets: [Budget]
+    createdAt: Date!
   }
 
-  type Query {
-    classes: [Class]
+  type UserType {
+    _id: ID!
+    userType: String!
   }
+
+  type Category {
+    _id: ID!
+    category: String!
+  }
+
+  type Expense {
+    _id: ID!
+    description: String!
+    amount: Float!
+    expenseDate: Date!
+    category: Category!
+    company: String!
+    isRecurring: Boolean!
+    createdAt: Date!
+  }
+
+  type Budget {
+    _id: ID!
+    description: String!
+    amount: Float!
+    category: Category!
+    createdAt: Date!
+  }
+
+  type Mutation {
+    addUser(username:String!, email:String!, password:String!): Auth
+  }
+
 `;
 
 module.exports = typeDefs;
