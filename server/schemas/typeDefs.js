280f084ebe6b50bd07b8c7d14c52b9226c242bad
lalitakapadia <lalita.kapadia@gmail.com>
2024-03-14 19:53:51 +0000

    work in progress ExpenseList and next page will do Budget

--- server/schemas/typeDefs.js
+++ server/schemas/typeDefs.js
@@ -1,57 +1,59 @@
-const {gql} = require('apollo-server-express');
+// const {gql} = require('apollo-server-express');
 
-const typeDefs = gql`
-  type User {
-    _id: ID!
-    username: String!
-    email: String!
-    phone: String!
-    addressLine1: String
-    addressLine2: String
-    addressLine3: String
-    city: String
-    postcode: String
-    county: String
-    country: String
-    userType: UserType!
-    expenses: [Expense]
-    budgets: [Budget]
-    createdAt: Date!
-  }
+// const typeDefs = gql`
+//   type User {
+//     _id: ID!
+//     username: String!
+//     email: String!
+//     password: String!
+//     phone: String!
+//     addressLine1: String
+//     addressLine2: String
+//     addressLine3: String
+//     city: String
+//     postcode: String
+//     county: String
+//     country: String
+//     userType: UserType!
+//     expenses: [Expense]
+//     budgets: [Budget]
+//     createdAt: Date!
+//   }
 
-  type UserType {
-    _id: ID!
-    userType: String!
-  }
+//   type UserType {
+//     _id: ID!
+//     userType: String!
+//   }
 
-  type Category {
-    _id: ID!
-    category: String!
-  }
+//   type Category {
+//     _id: ID!
+//     category: String!
+//   }
 
-  type Expense {
-    _id: ID!
-    description: String!
-    amount: Float!
-    expenseDate: Date!
-    category: Category!
-    company: String!
-    isRecurring: Boolean!
-    createdAt: Date!
-  }
+//   type Expense {
+//     _id: ID!
+//     description: String!
+//     amount: Float!
+//     expenseDate: Date!
+//     category: Category!
+//     company: String!
+//     isRecurring: Boolean!
+//     createdAt: Date!
+//   }
 
-  type Budget {
-    _id: ID!
-    description: String!
-    amount: Float!
-    category: Category!
-    createdAt: Date!
-  }
+//   type Budget {
+//     _id: ID!
+//     description: String!
+//     amount: Float!
+//     category: Category!
+//     createdAt: Date!
+//   }
 
-  type Mutation {
-    addUser(username:String!, email:String!, password:String!): Auth
-  }
+//   type Mutation {
+//     addUser(username:String!, email:String!, password:String!): Auth
+//     login(email: String!, password: String!): Auth
+//   }
 
-`;
+// `;
 
-module.exports = typeDefs;
+// module.exports = typeDefs;
